b9ca4e01260a5549aa78c9200efc3c8e79f2b5e8
Juan Alessandrini <juanalessandrini34@gmail.com>
2022-03-14 22:38:41 -0300

    Finalizando

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log("sentite identificada en cada prenda")
+//Sintaxis y Código
+
+//Palabras
+//Variables
+
+
+//var let const
+
+//ES5 ECMA5
+//var
+
+//ES6 ECMA6
+//let const
+
+
+//var nombre = (asignador de valor) el valor a asignar
+
+//var nombre;
+//let nombre;
+//const nombre;
+
+//Tipos de valores
+
+
+//string -> cadenas de caracteres o valores de tipo texto -> lo vamos a escribir entre " " 
+
+let nombre = "Carla";
